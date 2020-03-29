f31d16c3cd90bc9efaa8d48a93c632e120477e86
Maurice Cooper <jc70240@gmail.com>
2020-03-29 05:38:16 -0500

    initial deploy

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 # initializes our app
 app = Flask(__name__)
 
-ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev':
     app.debug = True
@@ -12,8 +12,10 @@ if ENV == 'dev':
     # connection to Postgres                 #postgres,   username:password@localhost/database_name
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:jeonju123@localhost/restaurant_feedback'
 else:
+
+    # productuon database
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yxgxmedzznltne:2da5d036b511c53add231bf8c91adce5bde4387565c1435680d7c4439668c5c5@ec2-35-174-88-65.compute-1.amazonaws.com:5432/d73mc5h8pq4nn4'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -25,16 +27,16 @@ db = SQLAlchemy(app)
 class Feedback(db.Model):
     __tablename__ = 'feedback'
     id = db.Column(db.Integer, primary_key=True)
-    customer = db.Column(db.String(200), unique=True)
-    email = db.Column(db.Text())
+    customer = db.Column(db.String(100), unique=True)
+    customer_email = db.Column(db.Text())
     waiter = db.Column(db.String(50))
-    rating = db.Column(db.Integer)
+    rating = db.Column(db.Text)
     comments = db.Column(db.Text())
 
     # constructor/initializer
-    def __init__(self, customer, email, waiter, rating, comments):
+    def __init__(self, customer, customer_email, waiter, rating, comments):
         self.customer = customer
-        self.email = email
+        self.customer_email = customer_email
         self.waiter = waiter
         self.rating = rating
         self.comments = comments
@@ -59,7 +61,17 @@ def submit():
         # validation that customer enters data into fields
         if customer == '' or waiter == '':
             return render_template('index.html', message='Please enter required fields')
-        return render_template('success.html')
+
+        # checks to make sure that customer doesnt already exist
+        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
+            # add data to database
+            data = Feedback(customer, customer_email, waiter, rating, comments)
+            db.session.add(data)
+            db.session.commit()
+            send_mail(customer, customer_email, waiter, rating, comments)
+
+            return render_template('success.html')
+        return render_template('index.html', message='You have already submitted feedback!')
 
 
 if __name__ == '__main__':
